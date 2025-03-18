4428b7ea8a1ef441928d5c4ed97764d7d7c3a764
AnnBrgn <bragina.anka@gmail.com>
2025-03-18 13:31:27 +1000

    init

---
+++ RailWayMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RailWayMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
